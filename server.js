38892dd9c51db178cdbea17afcffe35b3a71831b
GauravRteli <gauravteli134@gmail.com>
2023-09-27 01:27:49 +0530

    did started the mongo connection before listening to the server

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT;
 
-
-require("./Database/db");
+const mongoose = require("mongoose");
+// mongoose connection '
+const url = process.env.MONGODB_URL;
+const connectionparams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/FileRoutes"))
+app.use(require("./routes/FileRoutes"));
+const connectionDB = async () => {
+  try{
+    const conn = await mongoose.connect(url, connectionparams);
+    console.log("Mongo DB database connected");
+  }catch(error){
+    console.log("error occured");
+    process.exit(1);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+connectionDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
