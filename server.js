5d0667c59b1b0607332c68b689e0fb536e784064
GauravRteli <gauravteli134@gmail.com>
2023-09-27 02:21:11 +0530

    Changed the limit of payload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3001 || process.env.PORT;
@@ -24,6 +25,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(require("./routes/FileRoutes"));
+app.use(bodyParser.json({ limit: "10000kb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "10000kb", extended: true }));
 const connectionDB = async () => {
   try{
     const conn = await mongoose.connect(url, connectionparams);
