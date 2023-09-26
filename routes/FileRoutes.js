ebb62fb20e769f7a75bf10e6b760e7f2f423c64f
GauravRteli <gauravteli134@gmail.com>
2023-09-27 02:33:45 +0530

    middleware set

--- routes/FileRoutes.js
+++ routes/FileRoutes.js
@@ -8,7 +8,9 @@ const cors = require("cors");
 
 router.post(
   "/upload",
-  upload.fields([{ name: "image" }, { name: "video" }]), cors(),
+  upload.fields([{ name: "image" }, { name: "video" }]), cors({
+    origin: "http://localhost:3000"
+  }),
   async (req, res) => {
     // Accessing the uploaded 'thumbnail' and 'video' files via req.files
     if (!req.files || !req.files["image"] || !req.files["video"]) {
