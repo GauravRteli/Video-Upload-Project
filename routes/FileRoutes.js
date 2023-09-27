d494f30843571789e2e2513d82aa5cd395a53618
GauravRteli <gauravteli134@gmail.com>
2023-09-27 09:49:43 +0530

    make the file premissions open

--- routes/FileRoutes.js
+++ routes/FileRoutes.js
@@ -6,11 +6,35 @@ const VideoSchema = require("../Models/VideoSchema");
 const UploadToDB = require("../Utils/UploadsDB");
 const cors = require("cors");
 
-
+const uploadFileToCloudinary = async (filePath, resourceType) => {
+  try {
+    const result = await cloudinary.v2.uploader.upload(filePath, {
+      resource_type: resourceType,
+      chunk_size: 6000000,
+      eager: [
+        { width: 300, height: 300, crop: "pad", audio_codec: "none" },
+        {
+          width: 160,
+          height: 100,
+          crop: "crop",
+          gravity: "south",
+          audio_codec: "none",
+        },
+      ],
+      eager_async: true,
+    });
+    return result;
+  } catch (error) {
+    console.error(`Error uploading file to Cloudinary: ${error}`);
+    throw error;
+  }
+};
 
 router.post(
   "/upload",
-  upload.fields([{ name: "image" }, { name: "video" }]), async (req, res) => {
+  upload.fields([{ name: "image" }, { name: "video" }]),
+  async (req, res) => {
+    console.log("Called")
     // Accessing the uploaded 'thumbnail' and 'video' files via req.files
     if (!req.files || !req.files["image"] || !req.files["video"]) {
       return res
@@ -19,58 +43,30 @@ router.post(
     }
     const title = req.body.title;
     const description = req.body.description;
-    // accessing the 'thumbnail' and 'video' files' information and perform further processing
-    const thumbnailFile = req.files["image"][0];
-    const videoFile = req.files["video"][0];
 
-    fs.chmodSync(thumbnailFile.path, 0o777);
-    fs.chmodSync(videoFile.path, 0o777);
-    // logic for handling the files here
     try {
+      const thumbnailFile = req.files["image"][0];
+      const videoFile = req.files["video"][0];
 
-      var Fileres;
-      await cloudinary.v2.uploader
-        .upload(thumbnailFile.path, {
-          resource_type: "image",
-          chunk_size: 6000000,
-          eager: [
-            { width: 300, height: 300, crop: "pad", audio_codec: "none" },
-            {
-              width: 160,
-              height: 100,
-              crop: "crop",
-              gravity: "south",
-              audio_codec: "none",
-            },
-          ],
-          eager_async: true,
-        })
-        .then((result) => (Fileres = result));
+      // Upload the thumbnail image to Cloudinary
+      const thumbnailResult = await uploadFileToCloudinary(
+        thumbnailFile.path,
+        "image"
+      );
 
-      var Videores;
-      await cloudinary.v2.uploader
-        .upload(videoFile.path, {
-          resource_type: "video",
-          chunk_size: 6000000,
-          eager: [
-            { width: 300, height: 300, crop: "pad", audio_codec: "none" },
-            {
-              width: 160,
-              height: 100,
-              crop: "crop",
-              gravity: "south",
-              audio_codec: "none",
-            },
-          ],
-          eager_async: true,
-        })
-        .then((result) => (Videores = result));
+      // Upload the video file to Cloudinary
+      const videoResult = await uploadFileToCloudinary(videoFile.path, "video");
 
-        const result = await UploadToDB(title, description, Fileres.secure_url, Videores.secure_url);
+      const result = await UploadToDB(
+        title,
+        description,
+        thumbnailResult.secure_url,
+        videoResult.secure_url
+      );
 
       // Respond to the client once both uploads are complete
       res.status(200).send({
-        data : result
+        data: result,
       });
     } catch (error) {
       // Handle any errors that occur during the uploads or processing
@@ -80,32 +76,32 @@ router.post(
   }
 );
 
-router.get("/getAllVideos", async (req, res) =>{
+router.get("/getAllVideos", async (req, res) => {
   console.log("called");
   const data = await VideoSchema.find({});
-  if(data){
-      res.status(200).send({
-          data : data
-      });
-  }else{
-      res.status(400).send({
-          message: "video not found !"
-      });
+  if (data) {
+    res.status(200).send({
+      data: data,
+    });
+  } else {
+    res.status(400).send({
+      message: "video not found !",
+    });
   }
-})
-router.post("/getVideo", async (req, res) =>{
+});
+router.post("/getVideo", async (req, res) => {
   console.log("called");
-  console.log(req.body)
-    const data = await VideoSchema.findById(req.body.id);
-    if(data){
-        res.status(200).send({
-            data : data
-        });
-    }else{
-        res.status(400).send({
-            message: "video not found !"
-        });
-    }
-})
+  console.log(req.body);
+  const data = await VideoSchema.findById(req.body.id);
+  if (data) {
+    res.status(200).send({
+      data: data,
+    });
+  } else {
+    res.status(400).send({
+      message: "video not found !",
+    });
+  }
+});
 
 module.exports = router;
